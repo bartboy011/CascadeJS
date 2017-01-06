4c21bd4ae9e18c313b02a68380247f92c9c0900b
Joshua Bartlett <joshua.r.bartlett@gmail.com>
2017-01-06 12:24:55 -0600

    Restructure option input

--- cascade.js
+++ cascade.js
@@ -2,11 +2,15 @@ function Cascade (element, options) {
   // set defaults
   this.element = element; // element to segment and animate
   this.children = this.element.childNodes; // pre-segment child nodes
-  this.options = options || {};
-  this.class_prefix = this.options.class_prefix || 'ripple char';
-  this.tag_name = this.options.tag_name || 'span';
-  this.effect = this.options.effect || 'fadeUp';
-  this.total_time = this.options.total_time || 0.5;
+
+  function set_defaults (options) {
+    var opts             = options || {};
+    this.class_prefix    = opts.class_prefix || 'ripple char';
+    this.tag_name        = opts.tag_name || 'span';
+    this.effect          = opts.effect || 'fadeUp';
+    this.total_time      = opts.total_time || 0.5;
+    this.should_fragment = opts.should_fragment != null ? opts.should_fragment : true;
+  }
 
   // take a string inside an element, split it, wrap them
   // in their own tags, then inject the whole thing back
@@ -39,11 +43,11 @@ function Cascade (element, options) {
   this.flow = function (options) {
     // set defaults
     var opts = options || {};
-    var should_fragment = opts.should_fragment != null ? opts.should_fragment : true;
+    set_defaults(opts);
 
     // split the text into spans if segmentation
     // hasn't been performed yet
-    if (should_fragment) { this.fragment(); }
+    if (this.should_fragment) { this.fragment(); }
 
     // iterate through spans and animate them
     var array = this.children,
@@ -57,7 +61,11 @@ function Cascade (element, options) {
     }
   }.bind(this);
 
-  this.fragment = function () {
+  this.fragment = function (options) {
+    // set defaults
+    var opts = options || {};
+    set_defaults(opts);
+
     var children = this.children;
     if (children.length > 1) {
       console.error('CascadeJS Error - error in parent element:\n Please limit the amount children of this element to 1 (population control and all that).');
